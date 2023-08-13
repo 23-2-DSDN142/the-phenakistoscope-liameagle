2818e81a6a53fea8cb46047a87c8ea20c494e76f
liam <canoflime@gmail.com>
2023-08-14 11:27:28 +1200

    init commit
    flare layer, bang layer and basic movement

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -10,40 +10,32 @@ function setup_pScope(pScope){
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 17,23,60);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(flare);
+  layer1.mode( SWIRL(1) );
+  layer1.set_boundary( 0, 700 );
 
-  var layer2 = new PLayer(squares);
+  var layer2 = new PLayer(bang);
   layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  layer2.set_boundary( 700, 1000 );
+
 }
 
-function faces(x, y, animation, pScope){
+function flare(x, y, animation, pScope){
   
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
+  scale(animation.frame*1);
+  fill(255,160,180);
+  ellipse(0,0,80,80); // draw glow
+  fill(255);
+  ellipse(0,0,50,50); // draw flare
+  
 }
 
-function squares(x, y, animation, pScope){
+function bang(x,y,animation, pScope){
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+  //at the moment drawing an ellipse and scaling in a wave func to simulate an "explosion"
+  //TODO: replace with still image that expands or frame animation of explosion
+  ellipse(0,-700,animation.wave(1)*500,animation.wave(1)*500);//draw bang
+  
 }
