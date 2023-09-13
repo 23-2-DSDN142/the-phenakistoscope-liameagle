48eb9930962eb0617993636dcd35e0e5b2f83e12
liam <canoflime@gmail.com>
2023-09-13 20:44:38 +1200

    initial implementation of image animation framework
    forgot to commit changes, 4/9 monday sept

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -6,6 +6,8 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+
+  pScope.load_image("heart_circle","png")
 }
 
 function setup_layers(pScope){
@@ -25,17 +27,18 @@ function setup_layers(pScope){
 function flare(x, y, animation, pScope){
   
   scale(animation.frame*1);
-  fill(255,160,180);
+  fill(animation.wave(7)*255,animation.wave(7)*160,animation.wave(7)*180);//trying to flicker the glow colour
   ellipse(0,0,80,80); // draw glow
   fill(255);
   ellipse(0,0,50,50); // draw flare
-  
+
 }
 
 function bang(x,y,animation, pScope){
 
   //at the moment drawing an ellipse and scaling in a wave func to simulate an "explosion"
   //TODO: replace with still image that expands or frame animation of explosion
-  ellipse(0,-700,animation.wave(1)*500,animation.wave(1)*500);//draw bang
-  
+  pScope.draw_image('heart_circle',0,0);//draw bang
 }
+
+//TODO: add twinkling stars in the background..
